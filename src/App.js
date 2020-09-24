1d3443285082a4e72f3939fdeb954ca549465f75
James Calverley <james.calverley3@gmail.com>
2020-09-24 18:49:13 -0400

    formats nav items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import SearchPage from './components/SearchPage';
+import SavedPage from './components/SavedPage'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Google Books API</h1>
+    <Router>
+      <div className="App">
+      <NavBar />
+      <Route exact path={"/"} component={SearchPage}/>
+      <Route exact path={"/savedbooks"} component={SavedPage}/>
     </div>
+    </Router>
+    
   );
 }
 
