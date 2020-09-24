25e5676d5d0bbd1666c8b32f424b0c9086900f37
James Calverley <james.calverley3@gmail.com>
2020-09-24 17:31:13 -0400

    first commit - sets up React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Books API</h1>
+    </div>
+  );
+}
+
+export default App;
