68017dc14b93529d656c606602ca7eb43436747f
James Calverley <james.calverley3@gmail.com>
2020-09-24 23:37:47 -0400

    sets up api call for getting book search result, adds route and controller files

---
+++ controllers/handleBooks.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+async function getSearchResults(req,res){
+  console.log("[getSearchResults]".bold.blue);
+  console.log(`Incoming URL: ${req.url} M: ${req.method}`.blue);
+  try {
+  const apiKey = process.env.API_KEY;
+  const searchTerm = req.params.search;
+  console.log(`Search for: ${searchTerm}`.blue);
+  
+  const apiURL = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&orderBy=newest&key=${apiKey}`
+
+  const apiResult = await fetch(apiURL)
+    .then(res => res.json())
+    .catch(err => console.log("ERROR".red, err));
+
+  console.log(`Success -- Result Count ${apiResult.items.length}`.green);
+
+  
+    return res.send({ 
+      status: "Success", 
+      searchTerm: searchTerm,
+      count: apiResult.items.length,
+      data: apiResult })
+
+  } catch (err) {
+    return res.send({ status: "Error", message: "API CALL ERROR"})
+  }
+  
+};
+
+exports.getSearchResults = getSearchResults;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req,res) => {
-  res.send("google books api")
-});
+const bookSearch = require('./routes/books');
+app.use('/api/', bookSearch);
 
 app.listen(PORT, () => {
-  console.log(`App running at http://localhost:${PORT}`)
+  console.log(`App running at http://localhost:${PORT}`.bold.cyan)
 });
